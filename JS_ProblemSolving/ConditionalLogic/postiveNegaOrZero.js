a2eaa6bbf559ca40455060a9ddaf6e237465078a
SaurabhJadli <saurabhjadli7@protonmail.com>
2026-02-22 17:51:39 +0530

    check positive negative or zero

---
+++ JS_ProblemSolving/ConditionalLogic/postiveNegaOrZero.js
@@ -0,0 +1,14 @@
+const n = -10
+
+const PNZ = (n) => {
+    if (n > 0)
+        console.log(n + " is positive")
+
+    else if (n < 0)
+        console.log(n + " is negative")
+
+    else
+        console.log(n + " is zero")
+}
+
+PNZ(n)
