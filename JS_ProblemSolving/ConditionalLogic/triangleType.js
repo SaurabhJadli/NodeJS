8b5b7073e181843024f53694455e6d6cc9eeea65
SaurabhJadli <saurabhjadli7@protonmail.com>
2026-02-25 17:02:42 +0530

    triangle type'

--- JS_ProblemSolving/ConditionalLogic/triangleType.js
+++ JS_ProblemSolving/ConditionalLogic/triangleType.js
@@ -1,13 +1,19 @@
-const a = 10
-const b = 10
-const c = 10
+const a = 1
+const b = 4
+const c = 5
 
 const checkTri = (a, b, c) => {
     if (a ** 2 + b ** 2 == c ** 2)
         console.log('right triangle')
 
-    else if (a == b == c)
+    else if (a == b && b == c)
         console.log('equal triangle')
+
+    else if (a == b || a == c || b == c)
+        console.log('isosceles triangle')
+
+    else
+        console.log('scalene triangle')
 }
 
 checkTri(a, b, c)
